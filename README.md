54036f3631fea7437dd3249852f4a98fbe3efdd3
Bohdan <bogdan.loljoj@gmail.com>
2021-10-07 15:47:47 +0300

    complete project

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-flask-rest-mongo
+# Flask REST MongoDB
+
+## Descriptions
+
+Simple REST API to manage User model
+
+<br>
+
+### Installation
+
+Clone repo:
+```shell
+$ git clone https://github.com/bojandro/flast-rest-mongo
+```
+
+<br>
+
+Go to project dir:
+```shell
+$ cd flask-rest-mongo
+```
+
+<br>
+
+Run docker-compose:
+```shell
+$ docker-compose up --build
+```
+
+<br>
+
+### Docs
+
+`Swagger` API docs are available by going to `localhost:5000`
---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify, request
+from flask_restx import Api, Resource
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_mongoengine import MongoEngine
+import datetime
+import json
+
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app)
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'db',
+    'host': 'mongo',
+    'port': 27017
+}
+
+db = MongoEngine()
+db.init_app(app)
+name_space = api.namespace('api', description='API')
+
+
+class User(db.Document):
+    email = db.StringField()
+    name = db.StringField()
+    creating_date = db.DateTimeField(default=datetime.datetime.utcnow)
+    company_id = db.IntField()
+
+    def to_json(self):
+        return jsonify({"name": self.name,
+                        "email": self.email,
+                        "creating_date": self.creating_date,
+                        "company_id": self.company_id})
+
+@name_space.route("/user")
+class UserClass(Resource):
+    @name_space.doc(params={'name': 'name of a user'})
+    def get(self):
+        props = request.args
+        name = props['name']
+        user = User.objects(**props).first()
+        if not user:
+            return {'error': 'entry not found'}
+        else:
+            return user.to_json()
+
+    @name_space.doc(params={'name': 'name of a user',
+                            'email': 'email of a user',
+                            'company_id': 'company id'})
+    def post(self):
+        props = request.args
+        user = User(**props).save()
+        return {'message': 'created successfully'}
+
+    @name_space.doc(params={'name': 'name of a user',
+                            'email': 'email of a user',
+                            'company_id': 'company id'})
+    def put(self):
+        props = request.args
+        user = User.objects(name=props['name']).first()
+        if not user:
+            return jsonify({'error': 'entry not found'})
+        else:
+            user.update(**props)
+        return {'message': 'updated successfully'}
+
+    @name_space.doc(params={'name': 'name of a user'})
+    def delete(self):
+        props = request.args
+        user = User.objects(**props).first()
+        if not user:
+            return jsonify({'error': 'entry not found'})
+        else:
+            user.delete()
+        return {'message': 'deleted successfully'}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
